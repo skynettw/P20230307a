39ea0fb7181b7b1e58c7ae9458b8cba82c118802
skynettw <skynet.tw@gmail.com>
2023-03-07 11:40:54 +0800

    add ReadLine function

--- P20230307a/Program.cs
+++ P20230307a/Program.cs
@@ -15,15 +15,29 @@ namespace P20230307a
     {
         static void Main(string[] args)
         {
-            float w = 65.0F;
-            float h = 1.85F;
+            // float Weight = 65.0F, Height = 1.85F;
 
             // 下面這行是用來顯示文字
-            Console.Write("BMI:");
+            // Console.Write("BMI:");
             // 底下用來計算BMI，公式：BMI = weight / (height*height)
             // Console.WriteLine(Math.Round(60 / (1.9*1.9),2));
             // Console.WriteLine(60 / Math.Pow(1.9, 2));
-            Console.WriteLine(Math.Round(w / Math.Pow(h,2)));
+
+            double Weight, Height;
+
+            Console.Write("身高（公尺）：");
+            Height = Convert.ToDouble(Console.ReadLine());
+            Console.Write("體重（公斤）：");
+            Weight = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(Math.Round(Weight / Math.Pow(Height,2)));
+
+            //int x, y, z;
+            //x = y = z = 100;
+            //Console.WriteLine(x);
+
+
+
         }
     }
 }
