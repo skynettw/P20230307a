ecf2ce5fb7159eaca5a394e9d4241f13163bab24
skynettw <skynet.tw@gmail.com>
2023-03-07 11:00:10 +0800

    新增了兩個變數。

--- P20230307a/Program.cs
+++ P20230307a/Program.cs
@@ -15,11 +15,15 @@ namespace P20230307a
     {
         static void Main(string[] args)
         {
+            float w = 65.0F;
+            float h = 1.85F;
+
             // 下面這行是用來顯示文字
             Console.Write("BMI:");
             // 底下用來計算BMI，公式：BMI = weight / (height*height)
             // Console.WriteLine(Math.Round(60 / (1.9*1.9),2));
-            Console.WriteLine(60 / Math.Pow(1.9, 2));
+            // Console.WriteLine(60 / Math.Pow(1.9, 2));
+            Console.WriteLine(Math.Round(w / Math.Pow(h,2)));
         }
     }
 }
